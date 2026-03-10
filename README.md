2fe1d61b1d740d8bf9b3d367ac72664c84b1ba60
James Park <james.park@xanadu.ai>
2026-03-10 00:27:05 +0000

    rl training is hard...

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Learning RL through games. 
 
 The end game goal is to train a RL model on Starcraft Broodwar.
+
+# Space Invaders
+
+I actually decided to write this by hand because I wanted to learn DQL RL. I gotta say, not relying on AI (except for conceptual questions) really gets the knowledge the stick.
+- I'm currently on Vanilla DQN. It's so bad. I can't get it to converge. (Mar 9, 2026)
+- Next I'm going to try Double DQN, which is apparently a lot better.
--- models/atari/model.py
+++ models/atari/model.py
@@ -6,32 +6,38 @@ from collections import OrderedDict
 class DeepQNetwork(nn.Module):
     """Deep Q Network for selecting actions based off states"""
 
-    def __init__(self, img_height, img_width, action_space, num_frames, kernel_size = 4, stride=2):
+    def __init__(self, img_height, img_width, action_space, num_frames, stride=2):
         super().__init__()
         
-        self.kernel_size = kernel_size
         self._dummy_pass = torch.rand(1, num_frames, img_height, img_width)
-        self.stride = stride
 
         self.conv_layers = nn.Sequential(
             OrderedDict([
-                ('conv2d', nn.Conv2d(in_channels=num_frames, out_channels=4, kernel_size=self.kernel_size, stride=self.stride)),
-                ('silu', nn.SiLU()),
-                ('conv2d2', nn.Conv2d(in_channels=4, out_channels=4, kernel_size=self.kernel_size, stride=self.stride)),
-                ('silu2', nn.SiLU()),
-                ('conv2d3', nn.Conv2d(in_channels=4, out_channels=1, kernel_size=self.kernel_size, stride=self.stride)),
-                ('silu3', nn.SiLU()),
+                ('conv2d', nn.Conv2d(in_channels=num_frames, out_channels=8, kernel_size=8, stride=stride)),
+                ('silu', nn.GELU()),
+                ('conv2d2', nn.Conv2d(in_channels=8, out_channels=16, kernel_size=4, stride=stride)),
+                ('silu2', nn.GELU()),
+                ('conv2d3', nn.Conv2d(in_channels=16, out_channels=32, kernel_size=4, stride=stride)),
+                ('silu3', nn.GELU()),
+                ('conv2d4', nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=stride)),
+                ('silu4', nn.GELU()),
+                ('conv2d5', nn.Conv2d(in_channels=64, out_channels=64, kernel_size=2, stride=stride)),
+                ('silu5', nn.GELU()),
             ])    
         )
 
         _output_dummy = self.conv_layers(self._dummy_pass)
-        linear_dim = _output_dummy.shape[2] * _output_dummy.shape[3]
-
+        linear_dim = _output_dummy.flatten(start_dim=1).shape[1]
+        print(f"Linear Dim {linear_dim}")
         self.ff_layers = nn.Sequential(
             OrderedDict([
-                ('linear1', nn.Linear(linear_dim, linear_dim)),
+                ('linear1', nn.Linear(linear_dim, 512)),
                 ('gelu', nn.GELU()),
-                ('linear2', nn.Linear(linear_dim, action_space)),
+                ('linear2', nn.Linear(512, linear_dim)),
+                ('gelu2', nn.GELU()),
+                ('linear3', nn.Linear(linear_dim, 512)),
+                ('gelu3', nn.GELU()),
+                ('action_linear', nn.Linear(512, action_space)),
             ])
         )
 
